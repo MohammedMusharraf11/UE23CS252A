5b8ffa334a31304225484eced7121b5da8f2883e
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2024-09-24 16:28:48 +0530

    Fixed

--- queue_using_array.c
+++ queue_using_array.c
@@ -16,16 +16,34 @@ int insert(int x, int *q, int *r, int *f) {
     }
 }
 
-int deleteQ(int *front, int *rear, int *q){
-    if(*front>*rear) return -1;
-    return gq[(*front)++];
+int delete(int *q, int *f, int *r) {
+    if (*f == -1) {  // Queue is empty
+        printf("Queue is Empty");
+        return -1;
+    } else {
+        int x = q[*f];  // Store the value to be deleted
+        if (*f == *r) { // If the queue has only one element
+            *f = *r = -1;  // Reset both front and rear pointers
+        } else {
+            (*f)++;  // Move front to the next element
+        }
+        return x;  // Return the deleted element
+    }
 }
 
-void display(int front,int rear ,int * q){
-    if(*front>*rear){
-        printf("The Queue is Empty");
-        return;
+void display(int *q, int *r, int *f) {
+    if (*f == -1) {
+        printf("The Queue is Empty!!\n");  // Added new line for better formatting
+    } else {
+        int i = *f;
+        while (i <= *r) {  // Corrected condition to include elements from front to rear
+            printf("%d\t", q[i]);
+            i++;
+        }
+        printf("\n");  // Print new line after displaying all elements for better formatting
     }
+}
+
     
 }
 
